c3a1dacf84ca297f42e082f57dfe23a821d6a683
Juani Gallo <ji.gallo93@gmail.com>
2020-10-09 19:07:13 -0300

    testing main

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import Input from "./components/Input";
 import Button from "./components/Button";
 import TaskList from "./components/TaskList";
+import { useDispatch, useSelector, useStore } from "react-redux";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <div>
-          <Input />
-          <Button />
-        </div>
-        <TaskList />
-      </>
-    );
-  }
+function App() {
+  const taskValue = useSelector((state) => state.taskValue);
+  console.log(taskValue);
+  return (
+    <>
+      <div>
+        <Input />
+        <Button />
+      </div>
+      <TaskList />
+    </>
+  );
 }
+
 export default App;
